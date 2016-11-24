0808e3799b096871e130d3fbf2a96193ed4309bf
Nvolkovsky1 <N.volkovsky@gmail.com>
2016-11-24 23:29:00 +0300

    Add files via upload

---
+++ mechanics.js
@@ -0,0 +1,5 @@
+$('#five-button').click(function () {
+        console.log($(this).parent('img'));
+        $(this).parent().find('img').attr('src', 'http://klopik.com/uploads/posts/2015-02/1424727165_4.jpg');
+    });
+});
