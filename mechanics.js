627cece9045271831574d88829531b2e86364dfd
Nvolkovsky1 <N.volkovsky@gmail.com>
2016-11-25 00:09:13 +0300

    Add files via upload

--- mechanics.js
+++ mechanics.js
@@ -1,5 +1,6 @@
-$('#five-button').click(function () {
-        console.log($(this).parent('img'));
-        $(this).parent().find('img').attr('src', 'http://klopik.com/uploads/posts/2015-02/1424727165_4.jpg');
-    });
+$(document).ready(function () {
+	$('#photo_button').click(function () {
+		console.log($(this).parent('img'));
+		$(this).parent().find('img').attr('src', 'http://klopik.com/uploads/posts/2015-02/1424727165_4.jpg');
+	});
 });
